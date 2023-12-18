31ba606d33a241c8d908207dd9979ef56372f207
Katherine Hallissy Ayala <34048354+KatherineHallissyAyala@users.noreply.github.com>
2023-12-18 12:55:27 -0500

    created components, started nav

---
+++ appOld/posts/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// /posts
+
+export const metadata = {
+    title: 'Posts Index',
+};
+
+const page = () => {
+    return (
+        <div>POSTS</div>
+    )
+}
+
+export default page
