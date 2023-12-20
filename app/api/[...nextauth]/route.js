b9df92edc0a7adda00b3e0056b85e8c45ca48b24
Katherine Hallissy Ayala <34048354+KatherineHallissyAyala@users.noreply.github.com>
2023-12-20 11:29:44 -0500

    configured Google oAuth, started mongoose route

---
+++ app/api/[...nextauth]/route.js
@@ -0,0 +1,31 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+console.log({
+    clientId: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET
+});
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        })
+    ], 
+    async session({ session }){
+
+    },
+    async signIn({ profile }){
+        try {
+            // serverless -> lambda (opens only when it gets called)
+            
+        } catch (error) {
+
+        }
+
+
+    }
+})
+
+export { handler as GET, handler as POST }
